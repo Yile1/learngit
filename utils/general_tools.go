42896f0eb1fd8c44c7a2298b684acdf6166a355a
LvWuqian <louislvwq@gmail.com>
2021-11-04 22:53:57 +0800

    Update general_tools.go

--- utils/general_tools.go
+++ utils/general_tools.go
@@ -1,8 +1,8 @@
 package utils
 
 import (
-	"strings"
 	"strconv"
+	"strings"
 )
 
 
@@ -17,8 +17,8 @@ func IsStringInArray(target string, str_array []string) bool {
 
 func CompareVersion(Version1 []int, Version2 []int) bool {
 	// return true if Version1 < Version2
-	i:=0
-	for i<len(Version1) && i<len(Version2) {
+	i := 0
+	for i < len(Version1) && i < len(Version2) {
 		if Version1[i] < Version2[i] {
 			return true
 		}else if Version1[i] > Version2[i]{
@@ -26,12 +26,12 @@ func CompareVersion(Version1 []int, Version2 []int) bool {
 		}
 		i++
 	}
-	for i<len(Version1) {
+	for i < len(Version1) {
 		if Version1[i] != 0{
 			return false
 		}
 	}
-	for i<len(Version2) {
+	for i < len(Version2) {
 		if Version2[i] != 0{
 			return true
 		}
@@ -48,13 +48,13 @@ func IsUpdateVersionAvailable(updateVersionCode string, minUpdateVersionCode str
 	currVersion := make([]int, len(currVersionStr), cap(currVersionStr))
 	minVersion := make([]int, len(minVersionStr), cap(minVersionStr))
 	maxVersion := make([]int ,len(maxVersionStr), cap(maxVersionStr))
-	for i:=0; i<len(currVersionStr); i++ {
+	for i := 0; i < len(currVersionStr); i++ {
 		currVersion[i], _ = strconv.Atoi(currVersionStr[i])
 	}
-	for i:=0; i<len(minVersionStr); i++ {
+	for i := 0; i < len(minVersionStr); i++ {
 		minVersion[i], _ = strconv.Atoi(minVersionStr[i])
 	}
-	for i:=0; i<len(maxVersionStr); i++ {
+	for i := 0; i < len(maxVersionStr); i++ {
 		maxVersion[i], _ = strconv.Atoi(maxVersionStr[i])
 	}
 	flag1 := CompareVersion(minVersion, currVersion)
