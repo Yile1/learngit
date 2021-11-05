8d2730e4c286a2890c704ce2fcefd3fee142d283
LvWuqian <louislvwq@gmail.com>
2021-11-05 14:24:11 +0800

    Redis预热加载白名单

--- common/redis.go
+++ common/redis.go
@@ -1,9 +1,12 @@
 package common
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/spf13/viper"
+	"io"
+	"os"
 )
 
 var RedisClient *redis.Client
@@ -19,8 +22,53 @@ func RedisInit() {
 	if err != nil {
 		panic("redis ping error")
 	}
+
+	RDCPipeline := RedisClient.Pipeline()
+
+	fi, err := os.Open(viper.GetString("file.whiteList"))
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+
+	br := bufio.NewReader(fi)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		fmt.Println(string(a))
+		RDCPipeline.SAdd("WhiteList", string(a))
+	}
+	RDCPipeline.Exec()
 }
 
 func GetRdc() *redis.Client {
 	return RedisClient
+}
+
+func RedisTest() {
+	RDCPipeline := RedisClient.Pipeline()
+
+	fi, err := os.Open(viper.GetString("file.whiteList"))
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+
+	br := bufio.NewReader(fi)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		fmt.Println(string(a))
+		RDCPipeline.SAdd("WhiteListTest", string(a))
+	}
+	RDCPipeline.Exec()
+
+	fmt.Println(RedisClient.SMembers("WhiteListTest").Val())
+	fmt.Println(RedisClient.SIsMember("WhiteListTest", "789").Val())
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	InitConfig()
+
 	db := common.InitDB()
 	defer db.Close()
 
